d808b7b8e2736bd170607a5cb11db3e9e31f0901
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2025-03-27 02:18:34 +0700

    add events handler

--- src/contexts/ContractContext.tsx
+++ src/contexts/ContractContext.tsx
@@ -2,7 +2,9 @@
 
 import { createContext, useContext, useEffect, useState, ReactNode } from "react";
 import { ethers, formatUnits } from "ethers";
-import type { BrowserProvider, Signer, Contract } from "ethers";
+import type { BrowserProvider, Signer } from "ethers";
+import type { TEtreereumContract, TNFTreeContract } from "@/types/contract";
+import type { TEventData } from "@/types/event";
 import { useWallet } from "@/contexts/WalletContext";
 
 import treeCoinContract from "../../hardhat/artifacts/contracts/TreeCoin.sol/TreeCoin.json" with { type: "json" };
@@ -12,12 +14,11 @@ const TREE_COIN_ADDRESS = process.env.NEXT_PUBLIC_TREE_COIN_ADDRESS ?? "";
 const TREE_ADDRESS = process.env.NEXT_PUBLIC_TREE_ADDRESS ?? "";
 
 type TContractContext = {
-    treeCoinContract: Contract | null;
-    treeContract: Contract | null;
-    provider: BrowserProvider | null;
-    signer: Signer | null;
-    balance: string;
-    fetchBalance: () => Promise<void>;
+    etreereumContract: TEtreereumContract,
+    nftreeContract: TNFTreeContract,
+    events: TEventData[],
+    provider: BrowserProvider | null,
+    signer: Signer | null,
 }
 
 const ContractContext = createContext<TContractContext | undefined>(undefined);
@@ -26,59 +27,189 @@ export function ContractProvider({ children }: { children: ReactNode }) {
     const { accountAddress } = useWallet();
     const [provider, setProvider] = useState<BrowserProvider | null>(null);
     const [signer, setSigner] = useState<Signer | null>(null);
-    const [treeCoin, setTreeCoin] = useState<Contract | null>(null);
-    const [tree, setTree] = useState<Contract | null>(null);
-    const [balance, setBalance] = useState<string>("0");
+    const [events, setEvents] = useState<TEventData[]>([]);
+
+
+    const [etreereumContract, setEtreereumContract] = useState<TEtreereumContract>({
+        instance: null,
+        role: "user",
+        balance: "-"
+    });
+    const [nftreeContract, setNFTreeContract] = useState<TNFTreeContract>({
+        instance: null,
+        role: "user",
+        balance: "-",
+        plantedAt: 0,
+        latitude: 0,
+        longitude: 0,
+        metadataURI: ""
+    });
+
+    const fetchEvent = async () => {
+        if (!etreereumContract.instance || !nftreeContract.instance) return;
+
+        const [mintEvents, burnEvents, ownerEvents, transactionEvents] = await Promise.all([
+            nftreeContract.instance.queryFilter("MintTree", 0, "latest"),
+            nftreeContract.instance.queryFilter("BurnTree", 0, "latest"),
+            nftreeContract.instance.queryFilter("ChangeOwner", 0, "latest"),
+            etreereumContract.instance.queryFilter("NewTransaction", 0, "latest")
+        ]);
+
+        const allEvents: TEventData[] = [
+            ...(mintEvents ?? []).map(e => ({
+                type: "MintTree",
+                owner: e.args.owner,
+                plantedAt: Number(e.args.plantedAt),
+                metadataURI: e.args.metadataURI,
+                latitude: Number(e.args.latitude),
+                longitude: Number(e.args.longitude),
+                blockNumber: e.blockNumber
+            })),
+            ...(burnEvents ?? []).map(e => ({
+                type: "BurnTree",
+                owner: e.args.owner,
+                tokenId: Number(e.args.tokenId),
+                latitude: Number(e.args.latitude),
+                longitude: Number(e.args.longitude),
+                blockNumber: e.blockNumber
+            })),
+            ...(ownerEvents ?? []).map(e => ({
+                type: "ChangeOwner",
+                oldOwner: e.args.oldOwner,
+                newOwner: e.args.newOwner,
+                blockNumber: e.blockNumber
+            })),
+            ...(transactionEvents ?? []).map(e => ({
+                type: "NewTransaction",
+                sender: e.args.sender,
+                recipient: e.args.recipient,
+                amount: ethers.formatUnits(e.args.amount, 18),
+                blockNumber: e.blockNumber
+            }))
+        ];
+
+        const sortedEvents = allEvents.sort((a, b) =>  b.blockNumber - a.blockNumber).slice(0, 1000);
+        setEvents(sortedEvents);
+        console.log("Fetched events:", sortedEvents);
+    };
 
     useEffect(() => {
-        if (!window.ethereum) return;
-        const web3Provider = new ethers.BrowserProvider(window.ethereum);
-        setProvider(web3Provider);
+        const initContracts = async () => {
+            if (!window.ethereum) {
+                console.error("Ethereum provider not found!");
+                return;
+            }
 
-        if (accountAddress) {
-            web3Provider.getSigner().then((signer) => {
+            const web3Provider = new ethers.BrowserProvider(window.ethereum);
+            setProvider(web3Provider);
+
+            if (!accountAddress) return;
+
+            try {
+                const signer = await web3Provider.getSigner();
                 setSigner(signer);
 
-                const coinContract = new ethers.Contract(TREE_COIN_ADDRESS, treeCoinContract.abi, signer);
-                setTreeCoin(coinContract);
+                const etrContract = new ethers.Contract(TREE_COIN_ADDRESS, treeCoinContract.abi, signer);
+                const coinRole = await etrContract.getRoleName();
 
                 const nftContract = new ethers.Contract(TREE_ADDRESS, treeContract.abi, signer);
-                setTree(nftContract);
-            });
-        }
+                const nftRole = await nftContract.getRoleName();
+
+                const x = await nftContract.getTrees(accountAddress);
+
+                setEtreereumContract(prev => ({
+                    ...prev,
+                    instance: etrContract,
+                    role: coinRole
+                }));
+
+                setNFTreeContract(prev => ({
+                    ...prev,
+                    instance: nftContract,
+                    role: nftRole
+                }));
+
+
+            } catch (error) {
+                console.error("Error initializing contracts:", error);
+            }
+        };
+
+        initContracts();
     }, [accountAddress]);
 
-
-
     useEffect(() => {
-        if (!treeCoin) return;
-        
+        if (!etreereumContract.instance) return;
+
         const fetchBalance = async () => {
-            if (!treeCoin || !accountAddress) return;
+            if (!accountAddress) return;
             try {
-                const newBalance = await treeCoin.balanceOf(accountAddress);
-                setBalance(formatUnits(newBalance, 18));
+                const newBalance = await etreereumContract.instance?.getBalance(accountAddress);
+                const formattedBalance = formatUnits(newBalance, 18);
+                setEtreereumContract(prev => ({
+                    ...prev,
+                    balance: formattedBalance
+                }));
+
             } catch (error) {
                 console.error("Error fetching balance:", error);
             }
         };
 
         fetchBalance();
-        treeCoin.on("newTransaction", (sender, recipient, amount, event) => {
-            if (sender.toLowerCase() === accountAddress || recipient.toLowerCase() === accountAddress) {
-                console.log(`Transfer detected\n from ${sender}\nto: ${recipient}\namount: ${amount} `);
-                console.log("Event Data:", event);
-                fetchBalance();
-            }
-        });
+
+        etreereumContract.instance.on("NewTransaction", () => fetchBalance().then(() => fetchEvent()));
+
 
         return () => {
-            treeCoin.removeAllListeners("Transfer");
+            etreereumContract.instance?.removeAllListeners("NewTransaction");
         };
-    }, [treeCoin, accountAddress]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [accountAddress, etreereumContract.instance]);
+
+    useEffect(() => {
+        if (!nftreeContract.instance) return;
+
+        const fetchBalance = async () => {
+            if (!accountAddress) return;
+            try {
+                const newBalance = await nftreeContract.instance?.balanceOf(accountAddress);
+                setNFTreeContract(prev => ({
+                    ...prev,
+                    balance: newBalance.toString()
+                }));
+            } catch (error) {
+                console.error("Error fetching balance:", error);
+            }
+        };
+
+        fetchBalance();
+
+        nftreeContract.instance.on("MintTree", () => fetchBalance().then(() => fetchEvent()));
+        nftreeContract.instance.on("BurnTree", () => fetchBalance().then(() => fetchEvent()));
+        nftreeContract.instance.on("ChangeOwner", () => fetchBalance().then(() => fetchEvent()));
+
+        return () => {
+            nftreeContract.instance?.removeAllListeners("MintTree")
+                .then(() => nftreeContract.instance?.removeAllListeners("BurnTree"))
+                .then(() => nftreeContract.instance?.removeAllListeners("ChangeOwner"))
+        };
+
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [accountAddress, nftreeContract.instance]);
+
+
+    useEffect(() => {
+        if (!etreereumContract.instance || !nftreeContract.instance) return;
+        fetchEvent();
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [etreereumContract.instance, nftreeContract.instance]);
+
 
     return (
-        <ContractContext.Provider value={{ treeCoinContract: treeCoin, treeContract: tree, provider, signer, balance }}>
+        <ContractContext.Provider value={{ provider, signer, etreereumContract, nftreeContract, events }}>
             {children}
         </ContractContext.Provider>
     );
@@ -91,4 +222,3 @@ export function useContract() {
     }
     return context;
 }
-
