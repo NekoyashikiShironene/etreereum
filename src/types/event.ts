cac4e42b4733d12fb0a35cbcd6917cbe7115d216
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2025-03-27 01:11:39 +0700

    add event types

---
+++ src/types/event.ts
@@ -0,0 +1,23 @@
+export type TEventData = {
+    type: string;
+    owner?: string;
+    plantedAt?: number;
+    metadataURI?: string;
+    latitude?: number;
+    longitude?: number;
+    tokenId?: number;
+    oldOwner?: string;
+    newOwner?: string;
+    sender?: string;
+    recipient?: string;
+    amount?: string;
+    blockNumber: number;
+};
+
+
+export type HumanReadableEvent = {
+    message: string;
+    color: 'green' | 'red';
+    icon?: string; 
+    timestamp?: number; 
+}
