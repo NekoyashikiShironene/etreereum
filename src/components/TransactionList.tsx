433acbf80065de067062acb1c11a46cfb1f5e2c5
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2025-03-27 02:18:53 +0700

    implement working transactions list

--- src/components/TransactionList.tsx
+++ src/components/TransactionList.tsx
@@ -1,18 +1,29 @@
+import { useContract } from "@/contexts/ContractContext";
+import { useWallet } from "@/contexts/WalletContext";
+
+import { toHumanReadableEvents } from "@/utils/filter";
+
 export default function TransactionList() {
-    return (
-      <div className="bg-gray-100 rounded-xl p-6 shadow">
-        <h3 className="text-lg font-medium mb-2">Transaction History</h3>
-        <div className="flex flex-col gap-2">
-          <div className="flex justify-between text-green-700">
-            <span>+100 ECO (Tree Planting)</span>
-            <span className="text-sm text-gray-500">2 hours ago</span>
-          </div>
-          <div className="flex justify-between text-red-600">
-            <span>-60 ECO (Eco Store Purchase)</span>
-            <span className="text-sm text-gray-500">Yesterday</span>
-          </div>
-        </div>
+  const { accountAddress } = useWallet();
+  const { events } = useContract();
+
+  const filteredEvents = toHumanReadableEvents(events, accountAddress ?? '');
+
+  return (
+    <div className="bg-gray-100 rounded-xl p-6 shadow">
+      <h3 className="text-lg font-medium mb-2">Transaction History</h3>
+      <div className="flex flex-col gap-2">
+        {
+          filteredEvents.map((event, index) => (
+            <div key={index} className="flex justify-between text-green-700">
+              <span>{event.message}</span>
+              <span className="text-sm text-gray-500">2 hours ago</span>
+            </div>
+          ))
+        }
+
       </div>
-    );
-  }
-  
+    </div>
+  );
+
+}
