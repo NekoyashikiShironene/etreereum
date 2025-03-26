ec6b455953f42aa601284a3a5f1d4e5b7cdc198a
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2025-03-26 23:18:40 +0700

    complete functional requirements

--- hardhat/contracts/Tree.sol
+++ hardhat/contracts/Tree.sol
@@ -5,11 +5,14 @@ import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/access/AccessControl.sol';
 
-contract NFTree is ERC721, Ownable, AccessControl  {
+contract NFTree is ERC721, AccessControl  {
     uint256 private _nextTokenId;
     bytes32 public constant ADMIN = keccak256("ADMIN");
+    address private _owner;
 
-    event NewTree(address user, uint256 plantedAt, string metadataURI, int24 latitude, int24 longitude);
+    event MintTree(address owner, uint256 plantedAt, string metadataURI, int24 latitude, int24 longitude);
+    event BurnTree(address owner, uint256 tokenId, int24 latitude, int24 longitude);
+    event ChangeOwner(address oldOwner, address newOwner);
 
     struct Coordinates {
         int24 latitude;
@@ -25,14 +28,75 @@ contract NFTree is ERC721, Ownable, AccessControl  {
 
     mapping(uint256 => Tree) public treeData;
     mapping(address => uint256[]) public userOwnedTree;
+    
 
-    constructor(address owner) ERC721("NFTree", "NFT") Ownable(owner) {
+    constructor(address owner) ERC721("NFTree", "NFT") {
+        _owner = owner;
         _grantRole(DEFAULT_ADMIN_ROLE, owner);
         _grantRole(ADMIN, owner);
     }
 
     function supportsInterface(bytes4 interfaceId) public view override(ERC721, AccessControl) returns (bool) {
         return super.supportsInterface(interfaceId);
+    }
+    
+    function grantAdmin(address account) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        grantRole(ADMIN, account);
+    }
+
+    function revokeAdmin(address account) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(account != _owner, "Can not revoke owner from admin role");
+        revokeRole(ADMIN, account);
+    }
+
+    function getRoleName() public view returns(string memory) {
+        if (hasRole(DEFAULT_ADMIN_ROLE, msg.sender)) {
+            return "owner";
+        }
+        else if (hasRole(ADMIN, msg.sender)) {
+            return "admin";
+        }
+        else {
+            return "user";
+        }
+    }
+
+    function getTokenOwner(uint256 tokenId) public view returns(address) {
+        return ownerOf(tokenId);
+    }
+
+    function getTree(uint256 tokenId) public view returns(Tree memory) {
+        return treeData[tokenId];
+    }
+
+    function getTrees(address treeOwner) public view returns(Tree[] memory) {
+        uint256[] memory tokens = userOwnedTree[treeOwner];
+        Tree[] memory trees = new Tree[](tokens.length);
+
+        for (uint256 i=0; i<tokens.length; i++)
+            trees[i] = treeData[tokens[i]];
+
+        return trees;
+    }
+
+    function changeTreeOwner(uint256 tokenId, address newOwner) public {
+        require(getTokenOwner(tokenId) == msg.sender, "Sender must be owner of the token");
+
+        safeTransferFrom(msg.sender, newOwner, tokenId);
+        userOwnedTree[newOwner].push(tokenId);
+
+        // remove token from the old owner
+        uint256 oldOwnerTreesLength = userOwnedTree[msg.sender].length;
+        for (uint256 i=0; i<oldOwnerTreesLength; i++) {
+            if (userOwnedTree[msg.sender][i] == tokenId) {
+                userOwnedTree[msg.sender][i] = userOwnedTree[msg.sender][oldOwnerTreesLength - 1];
+                userOwnedTree[msg.sender].pop();
+                break;
+            }
+        }
+
+        emit ChangeOwner(msg.sender, newOwner);
+
     }
 
     function mintTree(
@@ -46,17 +110,18 @@ contract NFTree is ERC721, Ownable, AccessControl  {
         _safeMint(user, _nextTokenId);
         _nextTokenId++;
         userOwnedTree[user].push(_nextTokenId);
-        
-        emit NewTree(user, plantedAt, metadataURI, latitude, longitude);
+        emit MintTree(user, plantedAt, metadataURI, latitude, longitude);
     }
 
-    function getOwner(uint256 tokenId) public view returns(address) {
-        return ownerOf(tokenId);
+    function burnTree(uint256 tokenId) public onlyRole(ADMIN) {
+        address treeOwner = getTokenOwner(tokenId);
+        int24 latitude = treeData[tokenId].gpsLocation.latitude;
+        int24 longitude = treeData[tokenId].gpsLocation.longitude;
+
+        delete treeData[tokenId];
+        _burn(tokenId);
+
+        emit BurnTree(treeOwner, tokenId, latitude, longitude);
     }
 
-    function setAdmin(address account) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        _grantRole(ADMIN, account);
-    }
-
-    
 }
--- hardhat/contracts/TreeCoin.sol
+++ hardhat/contracts/TreeCoin.sol
@@ -1,45 +1,64 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+pragma solidity >=0.8.20;
 
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
-import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/access/AccessControl.sol';
 
-contract TreeCoin is ERC20, Ownable, AccessControl {
-    bytes32 public constant MINTER = keccak256("MINTER");
+contract TreeCoin is ERC20, AccessControl {
     bytes32 public constant ADMIN = keccak256("ADMIN");
+    address private _owner;
 
-    event newTransaction(address sender, address recipient, uint256 amount);
+    event NewTransaction(address sender, address recipient, uint256 amount);
 
-    constructor(address owner) ERC20("Etreereum", "ETR") Ownable(owner) {
+    constructor(address owner) ERC20("Etreereum", "ETR") {
+        _owner = owner;
+        _grantRole(DEFAULT_ADMIN_ROLE, owner);
+        _grantRole(ADMIN, owner);
         _mint(owner, 10000000 * 10 ** 18);
     }
 
-    function setAdmin(address account) public onlyOwner {
+    function grantAdmin(address account) public onlyRole(DEFAULT_ADMIN_ROLE) {
         grantRole(ADMIN, account);
+    }
+
+    function revokeAdmin(address account) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(account != _owner, "Can not revoke owner from admin role");
+        revokeRole(ADMIN, account);
+    }
+
+    function getRoleName() public view returns(string memory) {
+        if (hasRole(DEFAULT_ADMIN_ROLE, msg.sender)) {
+            return "owner";
+        }
+        else if (hasRole(ADMIN, msg.sender)) {
+            return "admin";
+        }
+        else {
+            return "user";
+        }
     }
 
     function getBalance(address account) public view returns(uint256) {
         return balanceOf(account);
     }
 
-    function transfer(address recipient, uint256 amount) override public returns(bool) {
+    function transfer(address recipient, uint256 amount) public override returns(bool) {
         bool res = super.transfer(recipient, amount);
-        emit newTransaction(msg.sender, recipient, amount);
-
+        emit NewTransaction(msg.sender, recipient, amount);
         return res;
     }
 
-    function giveReward(address recipient, uint256 amount) public onlyOwner() {
-        super.transfer(recipient, amount);
-        emit newTransaction(msg.sender, recipient, amount);
+    function giveReward(address recipient, uint256 amount) public onlyRole(ADMIN) {
+        approve(address(this), amount);
+        super.transferFrom(_owner, recipient, amount);
+        emit NewTransaction(msg.sender, recipient, amount);
     }
 
-    function mint(address account, uint256 amount) public onlyOwner {
+    function mint(address account, uint256 amount) public onlyRole(DEFAULT_ADMIN_ROLE) {
         _mint(account, amount);
     }
 
-    function burn(address account, uint256 amount) public onlyOwner {
+    function burn(address account, uint256 amount) public onlyRole(DEFAULT_ADMIN_ROLE) {
         _burn(account, amount);
     }
 }
