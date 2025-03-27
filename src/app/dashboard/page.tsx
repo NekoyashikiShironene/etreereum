14557ac6f27e94ed150b9337dc5a71cd1db9c543
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2025-03-27 18:22:43 +0700

    add role-based rendering

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,17 +5,9 @@ import { useContract } from '@/contexts/ContractContext';
 import { useWallet } from '@/contexts/WalletContext';
 import type { PlantingTree } from '@/types/Tree'
 
-import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableFooter,
-    TableHead,
-    TableHeader,
-    TableRow,
-} from "@/components/ui/table"
-import { Button } from '@/components/ui/button';
+
+import AdminPendingTable from '@/components/AdminPendingTable';
+import UserPendingTable from '@/components/UserPendingTable';
 
 export default function Page() {
     const { nftreeContract } = useContract();
@@ -29,10 +21,17 @@ export default function Page() {
 
     }
 
+    
+
     useEffect(() => {
         const fetchRequests = async () => {
+            if (!accountAddress) return;
             try {
-                const res = await fetch('http://localhost:3000/api/trees');
+                const endpoint = new URL(`${process.env.NEXT_PUBLIC_URL}/api/trees`);
+                if (role === 'user')
+                    endpoint.searchParams.set('user', accountAddress ?? '');
+
+                const res = await fetch(endpoint.toString());
                 if (!res.ok) throw new Error("Failed to fetch requests");
 
                 const requestData = await res.json();
@@ -45,41 +44,13 @@ export default function Page() {
         };
 
         fetchRequests();
-    }, []);
+    }, [accountAddress, role]);
 
     return (
         <div className='text-center'>
-            <h1 className="text-2xl font-bold mb-4">Admin Dashboard</h1>
             {
-                role !== "user" &&
-                <Table className='max-w-[1200px] mx-auto'>
-                    <TableCaption>A list of pending trees.</TableCaption>
-                    <TableHeader>
-                        <TableRow>
-                            <TableHead>Tree ID</TableHead>
-                            <TableHead>Type ID</TableHead>
-                            <TableHead>Location</TableHead>
-                            <TableHead>Action</TableHead>
-                        </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                        {requests.map((req, index) => (
-                            <TableRow key={index}>
-                                <TableCell className="font-medium text-left">{req.treeId}</TableCell>
-                                <TableCell className='text-left'>{req.typeId}</TableCell>
-                                <TableCell className='text-left'>Latitude {req.latitude}, Longitude {req.longitude}</TableCell>
-                                <TableCell className='text-left'>
-                                    <Button>
-                                        Mint
-                                    </Button>
-                                </TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
+                role !== "user" ? <AdminPendingTable planting={requests} /> : <UserPendingTable planting={requests} />
             }
-
-
         </div>
     );
 }
