6bff0ff56265b3db7424fa980080dc7fe7739011
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2025-03-27 18:19:25 +0700

    add localhost image url
    Co-authored-by: 6504062660053 <6504062660053@users.noreply.github.com>

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "localhost",
+        port: "3000"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
