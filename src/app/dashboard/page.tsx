7fe6784e86db0ee8fc3859bbf7a4af0b945669df
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2025-03-28 14:21:20 +0700

    add burn tree table

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,33 +3,27 @@
 import { useState, useEffect } from 'react';
 import { useContract } from '@/contexts/ContractContext';
 import { useWallet } from '@/contexts/WalletContext';
-import type { PlantingTree } from '@/types/Tree'
+import { PlantingTree } from '@/types/tree';
 
 
 import AdminPendingTable from '@/components/AdminPendingTable';
 import UserPendingTable from '@/components/UserPendingTable';
+import BurnTreeTable from '@/components/BurnTreeTable';
 
 export default function Page() {
     const { nftreeContract } = useContract();
-    const { accountAddress } = useWallet();
+    const { selectedAccount } = useWallet();
 
     const role = nftreeContract.role;
     const [requests, setRequests] = useState<PlantingTree[]>([]);
 
-    const approveTree = async (req: PlantingTree) => {
-        //mint tree
-
-    }
-
-    
-
     useEffect(() => {
         const fetchRequests = async () => {
-            if (!accountAddress) return;
+            if (!selectedAccount) return;
             try {
                 const endpoint = new URL(`${process.env.NEXT_PUBLIC_URL}/api/trees`);
                 if (role === 'user')
-                    endpoint.searchParams.set('user', accountAddress ?? '');
+                    endpoint.searchParams.set('user', selectedAccount ?? '');
 
                 const res = await fetch(endpoint.toString());
                 if (!res.ok) throw new Error("Failed to fetch requests");
@@ -44,12 +38,16 @@ export default function Page() {
         };
 
         fetchRequests();
-    }, [accountAddress, role]);
+    }, [selectedAccount, role]);
 
     return (
         <div className='text-center'>
             {
                 role !== "user" ? <AdminPendingTable planting={requests} /> : <UserPendingTable planting={requests} />
+            }
+
+            {
+                role !== "user" && <BurnTreeTable />
             }
         </div>
     );
