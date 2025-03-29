0202e112a26478e72658a4e64462734f7ff158bf
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2025-03-29 13:50:02 +0700

    add accounts select

--- src/components/WalletButton.tsx
+++ src/components/WalletButton.tsx
@@ -1,16 +1,18 @@
 "use client";
+import { useContract } from "@/contexts/ContractContext";
 import { useWallet } from "@/contexts/WalletContext";
 
 export default function WalletButton() {
     const { accounts, selectedAccount, connectWallet, selectAccount } = useWallet();
-
+    const { etreereumContract } = useContract();
+    const { balance } = etreereumContract;
     return (
         <div className="flex items-center gap-2">
             <button
                 onClick={connectWallet}
                 className="cursor-pointer bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg"
             >
-                {selectedAccount ? `Connected: ${selectedAccount.slice(0, 6)}...${selectedAccount.slice(-4)}` : "Connect Wallet"}
+                {selectedAccount ? `Connected: ${selectedAccount.slice(0, 6)}...${selectedAccount.slice(-4)} (${balance} ETR)` : "Connect Wallet"}
             </button>
 
             {accounts.length > 1 && (
