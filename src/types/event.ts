428d3ec05a75f6495fdf9b6b56c1008d55f1895d
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2025-03-29 18:18:09 +0700

    use comma

--- src/types/event.ts
+++ src/types/event.ts
@@ -1,23 +1,24 @@
 export type TEventData = {
-    type: string;
-    owner?: string;
-    plantedAt?: number;
-    metadataURI?: string;
-    latitude?: number;
-    longitude?: number;
-    tokenId?: number;
-    oldOwner?: string;
-    newOwner?: string;
-    sender?: string;
-    recipient?: string;
-    amount?: string;
-    blockNumber: number;
+    type: string,
+    owner?: string,
+    plantedAt?: number,
+    metadataURI?: string,
+    latitude?: number,
+    longitude?: number,
+    tokenId?: number,
+    oldOwner?: string,
+    newOwner?: string,
+    sender?: string,
+    recipient?: string,
+    from: string,
+    amount?: string,
+    blockNumber: number,
 };
 
 
 export type HumanReadableEvent = {
-    message: string;
-    color: 'green' | 'red';
-    icon?: string; 
-    timestamp?: number; 
+    message: string,
+    color: 'green' | 'red',
+    icon?: string,
+    timestamp?: number,
 }
