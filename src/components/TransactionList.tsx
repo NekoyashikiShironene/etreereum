f8e1be323560ceb6548484e3072d29336b5af179
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2025-03-29 13:50:50 +0700

    fix color display

--- src/components/TransactionList.tsx
+++ src/components/TransactionList.tsx
@@ -1,22 +1,25 @@
 import { useContract } from "@/contexts/ContractContext";
 import { useWallet } from "@/contexts/WalletContext";
-
+import { cn } from "@/lib/utils";
 import { toHumanReadableEvents } from "@/utils/filter";
 
 export default function TransactionList() {
-  const { accountAddress } = useWallet();
+  const { selectedAccount } = useWallet();
   const { events } = useContract();
 
-  const filteredEvents = toHumanReadableEvents(events, accountAddress ?? '');
+  const filteredEvents = toHumanReadableEvents(events, selectedAccount ?? '');
 
   return (
     <div className="bg-gray-100 rounded-xl p-6 shadow">
       <h3 className="text-lg font-medium mb-2">Transaction History</h3>
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-2 h-[16rem] overflow-auto">
         {
           filteredEvents.map((event, index) => (
-            <div key={index} className="flex justify-between text-green-700">
-              <span>{event.message}</span>
+            <div key={index} className={cn("flex justify-between", {
+              "text-green-700": event.color === "green",
+              "text-red-700": event.color === "red"
+            })}>
+              <span>{event.icon} {event.message}</span>
               <span className="text-sm text-gray-500">Block ID: {event.timestamp}</span>
             </div>
           ))
