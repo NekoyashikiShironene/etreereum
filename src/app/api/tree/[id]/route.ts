d907f6942894e4ad403a74f0886db735fc81d9a7
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2025-03-30 22:46:54 +0700

    remove req

--- src/app/api/tree/[id]/route.ts
+++ src/app/api/tree/[id]/route.ts
@@ -2,7 +2,6 @@ import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/db/prisma";
 
 export async function GET(
-    req: NextRequest, 
     { params }: { params: Promise<{ id: string }> }
 ) {
     const { id: treeId } = await params;
@@ -17,4 +16,23 @@ export async function GET(
     })
 
     return NextResponse.json({ data: treeData })
+}
+
+export async function POST(
+    req: NextRequest,
+    { params }: { params: Promise<{ id: string }> }
+) {
+    const { height } = await req.json();
+    const { id: treeId } = await params;
+
+    const treeData = await prisma.planting.update({
+        where: {
+            treeId: Number(treeId)
+        },
+        data: {
+            height: Number(height)
+        }
+    })
+
+    return NextResponse.json({ data: treeData });
 }
