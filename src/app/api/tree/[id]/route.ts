69e7277df6420f277730fa233cc7670621e6b313
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2025-03-31 00:27:25 +0700

    add req params

--- src/app/api/tree/[id]/route.ts
+++ src/app/api/tree/[id]/route.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/db/prisma";
 
 export async function GET(
+    req: NextRequest,
     { params }: { params: Promise<{ id: string }> }
 ) {
     const { id: treeId } = await params;
