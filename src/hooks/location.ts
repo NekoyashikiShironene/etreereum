075aa2c105805c6f526192847c5887efa72728de
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2025-04-04 22:59:25 +0700

    handle error

--- src/hooks/location.ts
+++ src/hooks/location.ts
@@ -22,26 +22,35 @@ export function useLocation(random: boolean = true) {
     const [location, setLocation] = useState<TGpsLocation>({ latitude: 13, longitude: 100 });
 
     useEffect(() => {
-        if (random) {
-            const [latitude, longitude] = coordinates[Math.floor(Math.random() * coordinates.length + 1)];
+        try {
+            if (random) {
+                const [latitude, longitude] = coordinates[Math.floor(Math.random() * coordinates.length + 1)];
+                setLocation({ latitude, longitude });
+                return
+            }
+
+
+            function success(position: { coords: { latitude: number; longitude: number; }; }) {
+                setLocation({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude
+                })
+            }
+
+            function error() {
+
+            }
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(success, error);
+            } else {
+                console.log("Geolocation is not supported by this browser.");
+            }
+        } catch {
+            const [latitude, longitude] = coordinates[0];
             setLocation({latitude, longitude});
-            return
-        }
-             
-
-        function success(position: { coords: { latitude: number; longitude: number; }; }) {
-            setLocation({
-                latitude: position.coords.latitude,
-                longitude: position.coords.longitude
-            })
         }
 
-        function error() { }
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(success, error);
-        } else {
-            console.log("Geolocation is not supported by this browser.");
-        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return { location, setLocation };
